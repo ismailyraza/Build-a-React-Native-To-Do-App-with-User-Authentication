17bf305b364717b153c1fe0afd7c1be33193f52e
ismailyraza <98678304+ismailyraza@users.noreply.github.com>
2023-09-16 11:03:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-import { initializeApp } from '@react-native-firebase/app';
-import App from './App';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './AuthContext';
+import AppNavigator from './AppNavigator';
 
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AuthProvider>
+        <AppNavigator />
+      </AuthProvider>
+    </NavigationContainer>
+  );
+};
 
-import firebaseConfig from './firebaseConfig';
-
-
-initializeApp(firebaseConfig);
-
-
-AppRegistry.registerComponent(appName, () => App);
+export default App;
