ac9595d4d33cf72aef11fb1384cb3b281aba3f04
ismailyraza <98678304+ismailyraza@users.noreply.github.com>
2023-09-16 11:27:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
+// App.js
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './AuthContext';
-import AppNavigator from './AppNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <AuthProvider>
-        <AppNavigator />
-      </AuthProvider>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
